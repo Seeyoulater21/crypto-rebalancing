72189cbb1eb4c8d048e31d5110cc849786b55a45
Seeyoulater21 <95746834+Seeyoulater21@users.noreply.github.com>
2025-03-12 11:14:53 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/crypto-rebalaceing/', // Set base path for GitHub Pages deployment
+  base: '/crypto-rebalancing/', // Set base path for GitHub Pages deployment
   server: {
     host: "::",
     port: 8080,
